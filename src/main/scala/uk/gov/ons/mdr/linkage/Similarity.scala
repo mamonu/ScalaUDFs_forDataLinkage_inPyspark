220113ae91315f8d41680bdc6cd5c8983780d280
mamonu <thorosm@msn.com>
2019-10-19 00:09:20 +0100

    added CosineDistance and JaccardSimilarity

---
+++ src/main/scala/uk/gov/ons/mdr/linkage/Similarity.scala
@@ -0,0 +1,53 @@
+/**
+  * Simple Scala wrapper to turn an existing string similarity function into a UDF
+  */
+package uk.gov.ons.mdr.linkage
+
+import org.apache.spark.sql.api.java.UDF2
+import org.apache.commons.text.similarity
+
+class JaroWinklerSimilarity extends UDF2[String, String, Double] {
+  override  def call(left: String, right: String): Double = {
+    // This has to be instantiated here (i.e. on the worker node)
+    val distance = new similarity.JaroWinklerDistance()
+    distance(left, right)
+  }
+}
+
+object JaroWinklerSimilarity {
+  def apply(): JaroWinklerSimilarity = {
+    new JaroWinklerSimilarity()
+  }
+}
+
+
+
+class JaccardSimilarity extends UDF2[String, String, Double] {
+  override  def call(left: String, right: String): Double = {
+    // This has to be instantiated here (i.e. on the worker node)
+    val distance = new similarity.JaccardSimilarity()
+    distance(left, right)
+  }
+}
+
+object JaccardSimilarity {
+  def apply(): JaccardSimilarity = {
+    new JaccardSimilarity()
+  }
+}
+
+
+
+class CosineDistance extends UDF2[String, String, Double] {
+  override  def call(left: String, right: String): Double = {
+    // This has to be instantiated here (i.e. on the worker node)
+    val distance = new similarity.CosineDistance()
+    distance(left, right)
+  }
+}
+
+object CosineDistance {
+  def apply(): CosineDistance = {
+    new CosineDistance()
+  }
+}
