dcedfd37222fe95c5cd345a44dbea2a30f4975d8
mamonu <thorosm@msn.com>
2019-10-18 15:24:52 +0100

    similarity/distance

--- src/main/scala/uk/gov/ons/mdr/linkage/JaroWinklerSimilarity.scala
+++ src/main/scala/uk/gov/ons/mdr/linkage/JaroWinklerSimilarity.scala
@@ -1,12 +1,12 @@
 /**
-  * Simple Scala wrapper to turn an existing string distance function into a UDF
+  * Simple Scala wrapper to turn an existing string similarity function into a UDF
   */
 package uk.gov.ons.mdr.linkage
 
 import org.apache.spark.sql.api.java.UDF2
 import org.apache.commons.text.similarity
 
-class JaroWinklerDistance extends UDF2[String, String, Double] {
+class JaroWinklerSimilarity extends UDF2[String, String, Double] {
   override  def call(left: String, right: String): Double = {
     // This has to be instantiated here (i.e. on the worker node)
     val distance = new similarity.JaroWinklerDistance()
@@ -14,8 +14,8 @@ class JaroWinklerDistance extends UDF2[String, String, Double] {
   }
 }
 
-object JaroWinklerDistance {
-  def apply(): JaroWinklerDistance = {
-    new JaroWinklerDistance()
+object JaroWinklerSimilarity {
+  def apply(): JaroWinklerSimilarity = {
+    new JaroWinklerSimilarity()
   }
 }
